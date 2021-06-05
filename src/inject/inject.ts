717527754a235a9158c3241995c9088522d354f5
Seysa <garion.g@hotmail.fr>
2021-06-05 13:15:37 +0200

    added no check on top of inject scripts instead of multiple ts ignore

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,13 +1,10 @@
+//@ts-nocheck
 function inject(): void {
-    /* eslint-disable @typescript-eslint/ban-ts-comment */
-    //@ts-ignore
     Element.prototype.__attachShadow = Element.prototype.attachShadow;
     Element.prototype.attachShadow = function () {
-        //@ts-ignore
         const toReturn = this.__attachShadow({ mode: "open" });
         setTimeout(() => {
             const allCanvas = toReturn.querySelector("div").querySelectorAll("canvas");
-            //@ts-ignore
             allCanvas.forEach((canvas) => {
                 try {
                     canvas.getContext("2d").getImageData(0, 0, 0, 0);
