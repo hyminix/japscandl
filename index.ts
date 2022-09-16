dda3197bf51f96f3333cfdc428b5d270980e32e2
Seysa <garion.g@hotmail.fr>
2022-09-16 16:10:58 +0200

    added method to fetch japsca address from github

--- index.ts
+++ index.ts
@@ -1,28 +1,28 @@
-import browser from "./src/utils/browser"
-import chrome from "./src/utils/chrome"
-import fsplus from "./src/utils/fsplus"
-import mangaFormat from "./src/utils/mangaFormat"
-import compress from "./src/utils/compress"
+import browser from "./src/utils/browser";
+import chrome from "./src/utils/chrome";
+import compress from "./src/utils/compress";
+import fsplus from "./src/utils/fsplus";
+import mangaFormat from "./src/utils/mangaFormat";
 import * as types from "./src/utils/types";
+import * as website from "./src/utils/website";
 
 const utils = {
-    browser,
-    chrome,
-    fsplus,
-    mangaFormat,
-    compress: compress,
+  browser,
+  chrome,
+  fsplus,
+  compress,
+  mangaFormat,
+  website,
 };
 
 import getBrowser from "./src/utils/browser";
 
 const japscandl = {
-    getBrowser,
-    utils,
-    types
-}
+  getBrowser,
+  utils,
+  types,
+};
 export default japscandl;
-
-
 
 import Downloader from "./src/components/Downloader";
 import Fetcher from "./src/components/Fetcher";
---
+++ src/utils/website.ts
@@ -0,0 +1,9 @@
+export async function getJapscanFromGithub(): Promise<string> {
+  return (
+    await (
+      await fetch(
+        "https://raw.githubusercontent.com/japdl/japscandl/main/data/address.txt"
+      )
+    ).text()
+  ).trim();
+}
